17658751fde903dd91ce60c89282367b6f3298b9
Andy Zhou <andyzhou443@gmail.com>
2020-10-17 15:54:54 -0400

    Updated Token
    Changed token to current version

--- bot.py
+++ bot.py
@@ -7,4 +7,4 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('RadioBot is ready')
 
-client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.2nb80DzQXew57JggCiW1-gq00zc')
+client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
