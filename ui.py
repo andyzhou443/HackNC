1a202f7e206fde06fc85ea921844c9900020ae63
Lama Abed <lamaab@email.unc.edu>
2020-10-17 22:51:07 -0400

    saving input
    I added a button that when you click it it creats a file then uploades the frequency that the user want

--- ui.py
+++ ui.py
@@ -6,18 +6,27 @@ def _from_rgb(rgb):
     """
     return "#%02x%02x%02x" % rgb 
 
-root = Tk()
-root.geometry("270x250")
+def save():
+    a = entry_1.get()
+    with open(a + '.txt', 'w') as file_object:
+        file_object.write(a)
 
-enter_label = Label(root, text="Enter frequency: ", fg="grey")
-title = Label(root, text="Dicord Bot", bg=_from_rgb((128, 128, 255)), fg="white", width=15, height=1 )
-title1 = Label(root, text="", bg=_from_rgb((128, 128, 255)), fg="white", width=15, height=1 )
-title.grid(row=0, column=0)
-title1.grid(row=0, column=1)
-txt = Text(root, width=15, height=1, wrap=WORD)
+if __name__ == '__main__':
 
-entry_1 = Entry(root)
-entry_2 = Entry(root)
-enter_label.grid(row=5, column=0)
-txt.grid(row=5, column= 1)
-root.mainloop()
+    root = Tk()
+    root.geometry("270x250")
+
+    enter_label = Label(root, text="Enter frequency: ", fg="grey")
+    title = Label(root, text="Dicord Bot", bg=_from_rgb((128, 128, 255)), fg="white", width=15, height=1 )
+    title1 = Label(root, text="", bg=_from_rgb((128, 128, 255)), fg="white", width=15, height=1 )
+    title.grid(row=0, column=0)
+    title1.grid(row=0, column=1)
+
+    entry_1 = Entry(root, width=15)
+    enter_label.grid(row=5, column=0)
+    entry_1.grid(row=5, column= 1)
+
+    tune_in = Button(root, text="Tune In", command=save)
+    tune_in.grid(row=6, column=1)
+    root.mainloop()
+
