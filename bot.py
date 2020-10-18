54ef2760de2d99c90468c0aee9dc71799ae2cfe7
AndrewBracero <a.bracero777@gmail.com>
2020-10-17 23:27:52 -0400

    Added even more stuffs
    Added even more stuffs

--- bot.py
+++ bot.py
@@ -10,36 +10,49 @@
 #
 #
 # client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.2nb80DzQXew57JggCiW1-gq00zc')
-import matplotlib as matplotlib
-
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mlab
+import matplotlib.gridspec as gridspec
 from rtlsdr import *
-
-spectrograph = RtlSdr()
-
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.')
+spectrograph = RtlSdr()
+client = commands.Bot(command_prefix='.')
 
 
-@bot.event
+@client.event
 async def on_ready():
     print('RadioBot is ready to receive frequency')
 
 
-bot.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
+client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
 
 
-@bot.event
+@client.event
 async def on_message(message):
     global frequency
-    userInput = await bot.wait_for('message')
-    frequency = (int(userInput))
+    userInput = await client.wait_for('message')
+    frequency = (float(userInput))
 
 
-spectrograph.sample_rate = frequency
-spectrograph.center_freq = frequency * 45
-spectrograph.gain = 'auto'
+# This is to create a large enough data set for the
+# psuedo random generator for this plot to work
+np.random.seed(142069999)
 
-spectrograph.close()
+eigenvalue = 0.01
+ax = np.arange(0, 10, eigenvalue)
+n = np.random.rand(len(ax))
+by = np.exp(-ax / 0.05)
 
-psd()
+convolved = np.convolve(n, by) * eigenvalue
+convolved = convolved[:len(ax)]
+sides = 0.01 * np.sin(2 * np.pi * ax) + convolved
+
+# This plots the values for the power spectral density graph
+plt.subplot(200)
+plt.plot(ax, sides)
+plt.subplot(215)
+plt.psd(sides, 515, 1 / eigenvalue)
+
+plt.show()
