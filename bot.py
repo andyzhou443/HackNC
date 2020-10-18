e074ef2546d78be69b22e94b9b5b5f421c361bb5
Andy Zhou <andyzhou443@gmail.com>
2020-10-17 22:35:18 -0400

    Made Bot Join Work
    Made Bot Join Command Work

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-import asyncio
-import functools
-import itertools
 import discord
 from discord.ext import commands
 
@@ -10,11 +7,11 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('RadioBot is ready')
   
-@client.command(pass_context = True)
-async def summon (ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
-
+@client.command(pass_context=True)
+async def join(ctx):
+    if ctx.message.author.voice:
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
 
 client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
 
