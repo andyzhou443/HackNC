7fba0d041cf9736fe84e8596e51302dca6d87722
AndrewBracero <a.bracero777@gmail.com>
2020-10-17 21:14:52 -0400

    Added stuff
    Added stuff

--- bot.py
+++ bot.py
@@ -1,10 +1,45 @@
-import discord
+# import discord
+# from discord.ext import commands
+#
+# client = commands.Bot(command_prefix='.')
+#
+#
+# @client.event
+# async def on_ready():
+#     print('RadioBot is ready')
+#
+#
+# client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.2nb80DzQXew57JggCiW1-gq00zc')
+import matplotlib as matplotlib
+
+from rtlsdr import *
+
+spectrograph = RtlSdr()
+
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+bot = commands.Bot(command_prefix='.')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print('RadioBot is ready')
+    print('RadioBot is ready to receive frequency')
 
-client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.2nb80DzQXew57JggCiW1-gq00zc')
+
+bot.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
+
+
+@bot.event
+async def on_message(message):
+    global frequency
+    userInput = await bot.wait_for('message')
+    frequency = (int(userInput))
+
+
+spectrograph.sample_rate = frequency
+spectrograph.center_freq = frequency * 45
+spectrograph.gain = 'auto'
+
+spectrograph.close()
+
+psd()
