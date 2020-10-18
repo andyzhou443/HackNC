b16f0d686fdeb0e25b6787f8f040e8d901b49f23
Andy Zhou <andyzhou443@gmail.com>
2020-10-17 20:37:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,24 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('RadioBot is ready')
 
+@commands.command(name='summon')
+@commands.has_permissions(manage_guild=True)
+    async def _summon(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None):
+        """Summons the bot to a voice channel.
+        If no channel was specified, it joins your channel.
+        """
+
+        if not channel and not ctx.author.voice:
+            raise VoiceError('You are neither connected to a voice channel nor specified a channel to join.')
+
+        destination = channel or ctx.author.voice.channel
+        if ctx.voice_state.voice:
+            await ctx.voice_state.voice.move_to(destination)
+            return
+
+    ctx.voice_state.voice = await destination.connect()
+     
+
 client.run('NzY3MDY2MTIwMzA1MjQ2MjE4.X4sf_g.RjIpDVFJ3UKD0bRAPRE7dk1Vqe4')
+
+
